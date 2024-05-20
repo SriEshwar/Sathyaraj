aa6f13de37c753e3c27c0483c3cdcf0982d54d3c
sathyaraj2508 <sathyaraj2508@gmail.com>
2024-05-20 12:54:21 +0530

    updated

--- Angular/Task/src/app/account/account.component.ts
+++ Angular/Task/src/app/account/account.component.ts
@@ -15,14 +15,13 @@ export class AccountComponent {
   selectedAccountType: string = 'All';
 
   accountHolders: AccountHolder[] = [
-    new AccountHolder(123456789, "Sanjay", "Savings", 10000.0, "2024-05-15", "Canara-Bank"),
-    new AccountHolder(987654321, "Ram", "BusinessAccount", 20000.0, "2024-05-16", "SBI"),
-    new AccountHolder(987994321, "Deva", "SalaryAccount", 40000.0, "2024-08-16", "IndianOverseasBank"),
-    new AccountHolder(123456089, "Prakash", "Savings", 15000.0, "2024-06-15", "Canara-Bank"),
+    new AccountHolder(123456789, "Sathya", "Savings", 10000.0, "2024-05-15", "Canara-Bank"),
+    new AccountHolder(987654321, "Raj", "BusinessAccount", 20000.0, "2024-05-16", "SBI"),
+    new AccountHolder(987994321, "Selvi", "SalaryAccount", 40000.0, "2024-08-16", "IndianOverseasBank"),
+    new AccountHolder(123456089, "Tamil", "Savings", 15000.0, "2024-06-15", "Canara-Bank"),
     new AccountHolder(987654356, "Pravin", "BusinessAccount", 12000.0, "2024-07-16", "SBI"),
     new AccountHolder(987654334, "Kishore", "SalaryAccount", 4000.0, "2024-04-16", "IndianOverseasBank"),
     new AccountHolder(987654390, "Pradeep", "SalaryAccount", 700.0, "2024-09-16", "IndianOverseasBank"),
-
   ];
 
   get filteredAccountHolders(): AccountHolder[] {
